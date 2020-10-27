eb29f18a82a38aab8117426b307bcd964f34bfc7
hammzza1 <73281302+hammzza1@users.noreply.github.com>
2020-10-27 17:19:40 +0000

    Add files via upload
    Java work

---
+++ Maths.java
@@ -0,0 +1,16 @@
+
+public class Maths {
+	
+	public void addition(int a, int b) {
+		
+		int result;
+		result=a+b;
+		System.out.println("The Result is " + result);
+		
+	}
+
+	public void message(String name) {
+		System.out.println("welcome my friend " + name);
+		
+	}
+}
